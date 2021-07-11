ccd51b540f3384436f3dd6cc209ae044755ac687
Rashmikant <rashmikantshukla@gmail.com>
2021-07-11 16:35:17 +0100

    added Model deployment doc

--- Week_5/Cloud_Deployment/app.py
+++ Week_5/Cloud_Deployment/app.py
@@ -9,14 +9,13 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 #%%
-
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 #%%
 @app.route('/')
 def home():
     return render_template('index.html')
-#%%
+
 @app.route('/predict',methods=['POST'])
 def predict():
     '''
@@ -28,18 +27,8 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be Euro {}'.format(output))
-#%%
-#@app.route('/predict_api',methods=['POST'])
-#def predict_api():
-#    '''
-#    For direct API calls trought request
-#    '''
-#    data = request.get_json(force=True)
-#    prediction = model.predict([np.array(list(data.values()))])
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
-#    output = prediction[0]
-#    return jsonify(output)
-#%%
+
 if __name__ == "__main__":
     app.run(debug=True)
